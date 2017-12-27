7cb5742ed03d1d87a0a750a58dad49c6cd86cf17
yuppies <yuppies@vip.qq.com>
2017-12-27 14:37:56 +0800

    pre-release-1.1.4 20171227 commit

---
+++ WHJHFront/WebProjects/Game.Web/Mobile/Pay/LqReturn.aspx.cs
@@ -0,0 +1,36 @@
+﻿using Game.Entity.Treasure;
+using Game.Facade;
+using Game.Utils;
+using Game.Web.Helper;
+using System;
+using System.Web;
+
+namespace Game.Web.Mobile.Pay
+{
+    public partial class LqReturn : System.Web.UI.Page
+    {
+        /// <summary>
+        /// 页面加载
+        /// </summary>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+//                Log4Net.WriteInfoLog("零钱支付回掉进来了------query:" + GameRequest.Request.QueryString);
+
+                LQPay.Notify notify = new LQPay.Notify(GameRequest.Request.QueryString);
+                if (!string.IsNullOrEmpty(notify.Param))
+                {
+                    if (notify.IsChecked)
+                    {
+                        string[] pSplit = notify.Param.Split('|');
+                        string msg = "【" + pSplit[0] + "】充值成功！";
+                        Response.Redirect("/Mobile/Index.aspx?action=payreturn&msg="+ msg);
+                        return;
+                    }
+                }
+                Response.Redirect("/Mobile/Index.aspx?action=payreturn&msg=充值失败");
+            }
+        }
+    }
+}
