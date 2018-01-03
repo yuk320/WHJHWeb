c160573c9c7e73a7029781293ab65e2441d16e0e
yuppies <yuppies@vip.qq.com>
2018-01-03 09:36:52 +0800

    pre-release-1.1.5

--- WHJHFront/WebProjects/Game.Web/Mobile/MoblieVideoInfo.aspx.cs
+++ WHJHFront/WebProjects/Game.Web/Mobile/MoblieVideoInfo.aspx.cs
@@ -14,31 +14,34 @@ namespace Game.Web.Mobile
         /// </summary>
         protected void Page_Load(object sender, EventArgs e)
         {
+            Response.AddHeader("Access-Control-Allow-Origin", AppConfig.MoblieInterfaceDomain);
+
             if (IsPostBack) return;
             int userid = GameRequest.GetQueryInt("userid", 0);
             string time = GameRequest.GetQueryString("time");
             string sign = GameRequest.GetQueryString("sign");
             string vNumber = GameRequest.GetQueryString("vNumber");
 
-            if(userid<=0 || string.IsNullOrEmpty(time) || string.IsNullOrEmpty(sign)|| string.IsNullOrEmpty(vNumber))
+            if (userid <= 0 || string.IsNullOrEmpty(time) || string.IsNullOrEmpty(sign) ||
+                string.IsNullOrEmpty(vNumber))
             {
                 Response.Write("抱歉，参数错误");
                 return;
             }
-            if(!Regex.IsMatch(vNumber, @"^\d{5,}$"))
+            if (!Regex.IsMatch(vNumber, @"^\d{5,}$"))
             {
                 Response.Write("抱歉，参数格式异常");
                 return;
             }
             AjaxJsonValid ajv = Fetch.VerifySignData(userid + AppConfig.MoblieInterfaceKey + time, sign);
-            if(ajv.code == (int) ApiCode.VertySignErrorCode)
+            if (ajv.code == (int) ApiCode.VertySignErrorCode)
             {
                 Response.Write(EnumHelper.GetDesc(ApiCode.VertySignErrorCode));
                 return;
             }
 
             byte[] bt = FacadeManage.aideTreasureFacade.GetVideoDataByVNumber(vNumber);
-            if(bt == null)
+            if (bt == null)
             {
                 Response.Write("抱歉，存盘信息不存在");
                 return;
